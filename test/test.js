778580761554b758591565d39eeeb3e22ba15d07
Aaron Stacy <aaron.r.stacy@gmail.com>
2013-08-17 17:46:14 -0500

    fix the problem

--- test/test.js
+++ test/test.js
@@ -7,6 +7,6 @@ test('basic test', function() {
 
 test('can access the DOM', function() {
   var fixture = document.getElementById('qunit-fixture');
-  equal(fixture.innerText, 'this had better work.',
+  equal(fixture.innerText, 'this is the jam',
     'should be able to access the DOM.');
 });
